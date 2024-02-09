c64a0ad684b963ebc1dc4f3685ede0907e0149ed
GersonFeDutra <gerson.creative.des@gmail.com>
2024-02-09 19:17:30 -0300

    Sistemas Distribuídos: add ex02

---
+++ Programação Distribuída/RMI/ex02/Client.java
@@ -0,0 +1,16 @@
+/*
+Escreva um programa cliente/servidor, utilizando Java RMI em que:
+
+Servidor:
+    Deve oferecer um serviço para conversão de moedas, Euro para Real, Real Para Euro, Dolar para Real e Real para Dolar. 
+
+Cliente:
+    Solicitar uma conversão ao servidor.
+
+PS. Utilize os valores das moedas no dia da sua implementação.
+*/
+public class Client {
+    public static void main(String args[]) {
+
+    }
+}
