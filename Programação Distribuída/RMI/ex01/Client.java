0c6e5412f0be993b1a765700822bdc79b4466cc1
GersonFeDutra <gerson.creative.des@gmail.com>
2025-02-09 19:10:15 -0300

    Sistemas Distribuídos: add ex01

---
+++ Programação Distribuída/RMI/ex01/Client.java
@@ -0,0 +1,20 @@
+/*
+Escreva um programa cliente/servidor, utilizando Java RMI em que:
+O servidor deve implementar uma lista para armazenamento de mensagens;
+
+- Disponibilizar funções remotas para:
+   - Armazenar uma string na lista;
+   - Recuperar lista de mensagens;
+   - Retornar o IP do servidor;
+   - Retornar data e hora do servidor no formato: YYYY-MM-DD HH:MM
+
+Cliente
+- Invocar as funções remotas do servidor implementado.
+
+Enviar o link dos arquivos cliente.py e servidor.py de um repositório git.
+*/
+public class Client {
+    public static void main(String args[]) {
+
+    }
+}
